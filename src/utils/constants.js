e7f94380e856f75c5d840616c3924ffa3fe12db5
sarmasikerol <sarmasikerol@gmail.com>
2024-12-19 22:27:50 +0300

    transition added in sidebar

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+import { TiHome } from "react-icons/ti";
+import { FaUsers, FaChartArea, FaDiceD6 } from "react-icons/fa";
+import { IoIosPricetags } from "react-icons/io";
+
+export const list = [
+  { title: "Starter Pages", icon: <TiHome />, path: "" },
+  { title: "Dashboard", icon: <FaDiceD6 />, path: "" },
+  { title: "User Management", icon: <FaUsers />, path: "" },
+  { title: "Report Management", icon: <IoIosPricetags />, path: "" },
+  { title: "Event Management", icon: <FaChartArea />, path: "" },
+];
